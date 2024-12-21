663579f745987c656b16df17d0b0439f89a36843
Brad <bbodine1@gmail.com>
2024-12-20 21:33:19 -0600

    add core module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () => import('@enroll-ng/core').then((m) => m.CoreComponent),
+    pathMatch: 'full',
+  },
+];
